6cddc643a51d6a98993265273f1f66f8fc37da44
Ian Burke <iano.burke@gmail.com>
2018-02-09 15:16:31 +0000

    Added comments

--- Task3.rkt
+++ Task3.rkt
@@ -1,19 +1,22 @@
 #lang racket
 
-(define (lcycle l)
-  (if (null? l)
-      '()
-      (append (cdr l) (list (car l)))))
+; Take in a list and move the 1st element of the list to the end.
+(define (lcycle l) ; define our procedure and pass in list
+  (if (null? l) ; if list = null then..
+      '() ; output null
+      (append (cdr l) (list (car l))))) ; otherwise, take 1st element and append it to list
 
+; Take in a list and move the last element to the start of the list.
 (define (rcycle l)
-  (if (null? l)
-      '()
-      (append (cons (last l)(remove-last l)))))
+  (if (null? l) ; if list = null then..
+      '() ; output null
+      (append (cons (last l)(remove-last l))))) ; otherwise call the remove-last function to remove last element and append list to it
 
+; remove-last function to remove last element from list
 (define (remove-last l)
-  (if (null? (cdr l))
-        '()
-        (cons (car l) (remove-last (cdr l)))))
+  (if (null? (cdr l)) ; if 2nd element pair = null then..
+        '() ;output null
+        (cons (car l) (remove-last (cdr l))))) ; newly allocated list 
   
 
 
