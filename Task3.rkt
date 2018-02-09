79c05d67bfd7eb3e57387970c91c0693f08c3370
Ian Burke <iano.burke@gmail.com>
2018-02-09 14:03:56 +0000

    lcycle completed. rcycle not working

--- Task3.rkt
+++ Task3.rkt
@@ -1,5 +1,17 @@
 #lang racket
 
-(define (lcycle l))
+(define (lcycle l)
+  (if (null? l)
+      '()
+      (append (cdr l) (list (car l)))))
 
-(define (rcycle l))
+(define (rcycle l)
+  (if (null? l)
+      '()
+      (append (car l) (list (cdr l)))))
+
+
+
+(lcycle(list 1 2 3 4 5))
+
+(rcycle(list 1 2 3 4 5))
