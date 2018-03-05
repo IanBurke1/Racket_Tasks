bb8e57b2a2990c780cfa1917f91ceab419f3d7cc
Ian Burke <iano.burke@gmail.com>
2018-03-05 15:55:28 +0000

    Sum of list working

--- Task4/Task4.rkt
+++ Task4/Task4.rkt
@@ -1,15 +1,17 @@
 #lang racket
 
 (define (sublsum? l)
-  (cdr (combinations (cdr l))))
+  (combinations l))
 
 (sublsum? (list 1 2 3 4 -5))
 
-(display "Sum: ")
 
+
+; Function used to calculate sum of a list
 (define (sum li)
-  (if (null? li)
-      '()
-      (apply + li)))
+  (if (null? li) ; if list = null then..
+      '() ; return null, otherwise...
+      ; 'apply' is a racket func used to calculate the sum of elements in a list
+      (equal? (apply + li) 0))) ; then check if sum is = to 0 using 'equal' function
+
 (sum (list 1 2 3 4))
-  
