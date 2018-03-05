9965ad15383f5e804d4a9ea4c636343073450e3c
Ian Burke <iano.burke@gmail.com>
2018-03-05 15:09:20 +0000

    Sum of list working

--- Task4/Task4.rkt
+++ Task4/Task4.rkt
@@ -1,6 +1,15 @@
 #lang racket
 
 (define (sublsum? l)
-  (combinations l))
+  (cdr (combinations (cdr l))))
 
 (sublsum? (list 1 2 3 4 -5))
+
+(display "Sum: ")
+
+(define (sum li)
+  (if (null? li)
+      '()
+      (apply + li)))
+(sum (list 1 2 3 4))
+  
