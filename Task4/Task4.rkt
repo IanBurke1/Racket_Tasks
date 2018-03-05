396eb4d66610d77c2667e5e319928e5325da50f9
Ian Burke <iano.burke@gmail.com>
2018-03-05 22:25:36 +0000

    Task 4 completed

--- Task4/Task4.rkt
+++ Task4/Task4.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
 (define (sublsum? l)
-  (combinations l))
-
-(sublsum? (list 1 2 3 4 -5))
+  ; Combinations is a built in racket function that returns every combination of elements in a list
+  ; using filter, another racket func, to list out only the sublists that are true, in this case, equal to zero
+  (filter sum (combinations l)))
 
 
 
@@ -11,7 +11,10 @@
 (define (sum li)
   (if (null? li) ; if list = null then..
       '() ; return null, otherwise...
-      ; 'apply' is a racket func used to calculate the sum of elements in a list
+      ; 'apply' is a racket function used to calculate the sum of elements in a list
       (equal? (apply + li) 0))) ; then check if sum is = to 0 using 'equal' function
 
-(sum (list 1 2 3 4))
+; Pass in the list into our function
+(sublsum? (list 1 2 3 4 -5)) ; expected result: (2 3 -5) (1 4 -5)
+
+;(sublsum? (list 1 2 3 4 5)) ; expected result: '(())
