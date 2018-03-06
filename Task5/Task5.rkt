98f2cc92c79491ef37650ba13f4bd0f1f2847613
Ian Burke <iano.burke@gmail.com>
2018-03-06 12:26:25 +0000

    Added filter function

--- Task5/Task5.rkt
+++ Task5/Task5.rkt
@@ -2,7 +2,8 @@
 
 ; hamming-weight function takes in a list and returns the number of non-zero elements in it. 
 (define (hamming-weight? l)
-  (filter positive? l)) ; Using racket's filter function to filter the list to return a list of positive numbers not equal to zero
+  ; using apply function to sum the elements remaining in the list after being filtered.
+  (apply +(filter positive? l))) ; Using racket's filter function to filter the list to return a list of positive numbers not equal to zero
 
 
 (hamming-weight? (list 1 0 1 0 1 1 1 0))
