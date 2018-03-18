401fe749cdbbc71acddb84ef028778bbb54dd4cf
Ian Burke <iano.burke@gmail.com>
2018-03-18 22:37:40 +0000

    Task 6 completed

--- Task6/Task6.rkt
+++ Task6/Task6.rkt
@@ -2,10 +2,14 @@
 
 ; hamming-distance function used to calculate the hamming distance between 2 lists
 (define (hamming-distance l li)
-  ; if the length of l is equal to length of li then..
-  (if (= (length l)(length li))
-      #t 
-      #f))
+  (if (null? l) ; if list1 = null then..
+      0 ; output 0
+      (if (null? li) ; if list2 = null then..
+          0 ; output 0, otherwise..
+          (if (= (car l)(car li)) ; if the 1st element of list1 and list2 are equal then..
+              (hamming-distance (cdr l) (cdr li)) ; take the 2nd elements of the pair from both lists recursively
+              (+ 1 (hamming-distance (cdr l) (cdr li))))))) ; otherwise if not equal then add 1 until the end of the list
 
 ; Passing in our 2 lists
-(hamming-distance (list 1 0 1 0 1 1 1 0)(list 1 1 1 1 0 0 0 0))
+(hamming-distance (list 1 0 1 0 1 1 1 0)(list 1 1 1 1 0 0 0 0)) ; Expected result: 5
+(hamming-distance (list 1 1 1 0 0 0 1 0)(list 0 0 0 1 1 1 0 0)) ; Expected result: 7
