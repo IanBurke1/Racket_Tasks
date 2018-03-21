47f6f17309a563654c0cc42d1d04b71cb3094bc4
Ian Burke <iano.burke@gmail.com>
2018-03-21 22:46:00 +0000

    Task 8 completed

--- Task8/Task8.rkt
+++ Task8/Task8.rkt
@@ -7,7 +7,13 @@
   '() ; return null, otherwise..
   (if (not (= (length x)(length y)(length z))) ; if length of the lists are not equal then..
       '() ; return null, otherwise..
-      #t))
+      ; Map through the 3 lists and
+      ; check if elements in x is equal to 1 then..
+      ; add the corresponding element in y to a new list
+      ; otherwise if not equal to 1 (in this case equal to 0) then..
+      ; add the corresponding element in z to that new list.
+      (map (lambda (x y z)
+             (if (= x 1) y z)) x y z)))
 
 ; pass in 3 lists
 (chse (list 0 0 0 0 1 1 1 1)(list 0 0 1 1 0 0 1 1)(list 0 1 0 1 0 1 0 1)) ; Expected result: '(0 1 0 1 0 0 1 1)
