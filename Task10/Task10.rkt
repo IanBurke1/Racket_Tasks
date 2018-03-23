818fb28299cd7683e204863ca492f95775a5c109
Ian Burke <iano.burke@gmail.com>
2018-03-23 15:50:33 +0000

    Added recursion

--- Task10/Task10.rkt
+++ Task10/Task10.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+; ======== USING LAMBDA EXPRESSION ============================================================
 ; lstq function that takes in 2 lists l and m, subtract the corresponding element in m from l and square the result.
 (define (lstq l m)
   (apply + (map (lambda (l m) (sqrt (- m l) )) l m)))
@@ -9,3 +10,15 @@
 
 ; Pass in 2 lists
 (lstq (list 4.5 5.1 6.2 7.8) (list 1.1 -0.1 6.1 3.8)) ; Expected result: 54.61
+
+; ======== USING RECURSION =====================================================================
+
+(define (lstq2 l m)
+  (if (null? l)
+      '()
+      (cons (- (car l)(car m)) (lstq (cdr l)(cdr m)))))
+      
+
+; Pass in 2 lists
+(lstq2 (list 4.5 5.1 6.2 7.8) (list 1.1 -0.1 6.1 3.8)) ; Expected result: 54.61
+
