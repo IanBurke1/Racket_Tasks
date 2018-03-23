d7fe2be08be867e4e170e8f82390e6c9dd901678
Ian Burke <iano.burke@gmail.com>
2018-03-23 15:07:10 +0000

    Task 10 completed

--- Task10/Task10.rkt
+++ Task10/Task10.rkt
@@ -2,7 +2,10 @@
 
 ; lstq function that takes in 2 lists l and m, subtract the corresponding element in m from l and square the result.
 (define (lstq l m)
-  (apply + (map (lambda (l m) (* (- m l) )) l m)))
+  (apply + (map (lambda (l m) (sqrt (- m l) )) l m)))
+
+(define (sqrt x)
+  (* x x))
 
 ; Pass in 2 lists
 (lstq (list 4.5 5.1 6.2 7.8) (list 1.1 -0.1 6.1 3.8)) ; Expected result: 54.61
